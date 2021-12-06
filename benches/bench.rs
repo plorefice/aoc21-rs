f0d410b6cde3827d0eec98498a55d7c88646bcbc
Pietro Lorefice <pietro.lorefice@gmail.com>
2021-12-06 08:40:57 +0100

    day6: add benchmark

---
+++ benches/bench.rs
@@ -0,0 +1,16 @@
+use aoc21_rs::day06;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn day06(c: &mut Criterion) {
+    c.bench_function("day 06", |b| {
+        let input = day06::parse_input(include_str!("../inputs/day06.txt"));
+
+        b.iter(|| {
+            day06::part_1(black_box(input));
+            day06::part_2(black_box(input));
+        });
+    });
+}
+
+criterion_group!(benches, day06);
+criterion_main!(benches);
