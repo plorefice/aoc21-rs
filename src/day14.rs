ec66e88a35183c34c5631e8eedbc28b665edafde
Pietro Lorefice <pietro.lorefice@gmail.com>
2021-12-14 07:38:41 +0100

    day14: add both parts

---
+++ src/day14.rs
@@ -0,0 +1,75 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
+
+pub type Pairs = HashMap<(u8, u8), u64>;
+pub type Rules = HashMap<(u8, u8), u8>;
+pub type Letters = HashMap<u8, u64>;
+
+pub fn parse_input(input: &str) -> (Pairs, Letters, Rules) {
+    let (template, rules) = input.split_once("\n\n").unwrap();
+
+    let pairs = template
+        .bytes()
+        .tuple_windows()
+        .fold(HashMap::new(), |mut map, (a, b)| {
+            map.entry((a, b)).and_modify(|n| *n += 1).or_insert(1);
+            map
+        });
+
+    let letters = template.bytes().fold(HashMap::new(), |mut map, c| {
+        map.entry(c).and_modify(|n| *n += 1).or_insert(1);
+        map
+    });
+
+    let rules = rules
+        .lines()
+        .map(|l| {
+            let (left, right) = l.split_once(" -> ").unwrap();
+            (
+                (left.as_bytes()[0], left.as_bytes()[1]),
+                right.as_bytes()[0],
+            )
+        })
+        .collect();
+
+    (pairs, letters, rules)
+}
+
+pub fn part_1(input: (Pairs, Letters, Rules)) -> u64 {
+    solve(input, 10)
+}
+
+pub fn part_2(input: (Pairs, Letters, Rules)) -> u64 {
+    solve(input, 40)
+}
+
+pub fn solve((mut pairs, mut letters, rules): (Pairs, Letters, Rules), n: usize) -> u64 {
+    for _ in 0..n {
+        let mut next = HashMap::new();
+
+        for ((a, b), n) in pairs {
+            let rule = rules[&(a, b)];
+
+            letters.entry(rule).and_modify(|x| *x += n).or_insert(n);
+
+            next.entry((a, rule)).and_modify(|x| *x += n).or_insert(n);
+            next.entry((rule, b)).and_modify(|x| *x += n).or_insert(n);
+        }
+
+        pairs = next;
+    }
+
+    letters.values().max().unwrap() - letters.values().min().unwrap()
+}
+
+crate::solutions! {
+    p1 => {
+        part_1(parse_input(include_str!("../inputs/day14.txt"))),
+        3143
+    },
+    p2 => {
+        part_2(parse_input(include_str!("../inputs/day14.txt"))),
+        4110215602456
+    }
+}
